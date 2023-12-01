7b192d026e1cc28800e5bc6c38c4c6ea8e89529d
aziz <aziz@aziz.aziz>
2023-12-01 17:25:59 +0500

    first commit

---
+++ assets/SerialsId-6791ed77.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Single-fc3ef327.js";import{o as a,c as t,a as s}from"./index-799c92c4.js";const o={class:"main"},i={__name:"SerialsId",setup(c){return(r,_)=>(a(),t("main",o,[s(e,{type:"tv"})]))}};export{i as default};
